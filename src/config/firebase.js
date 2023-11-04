91d23f2b58cb987893971de265a9547450431d95
Alaleh-Mohseni <alalamohseni@gmail.com>
2023-11-04 13:07:41 +0330

    edite firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,18 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    apiKey: "AIzaSyDrJxaTNQLR7vIG6uIxIrFnk2R0e8col04",
-    authDomain: "form-validation-b449c.firebaseapp.com",
-    projectId: "form-validation-b449c",
-    storageBucket: "form-validation-b449c.appspot.com",
-    messagingSenderId: "1071737788258",
-    appId: "1:1071737788258:web:e14075abb84008b1b6fe3b"  
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
